c153823eac46aa6c5e497dfffb453b99943f416a
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-13 22:50:52 +0300

    adding a little bit of css

--- client/src/app/components/BookList.tsx
+++ client/src/app/components/BookList.tsx
@@ -9,10 +9,10 @@ interface BookListProps {
 const BookList: FC<BookListProps> = ({ title, books }) => {
   return (
     <section className="bg-white rounded-lg shadow-md border-2 border-white-600 h-[550px]">
-      <header className="h-20 bg-white rounded-lg shadow-md border-2 border-white-600 flex justify-center items-center">
+      <header className="h-18 bg-white rounded-lg shadow-md border-2 border-white-600 flex justify-center items-center p-3">
         <h1 className="text-xl font-bold">{title}</h1>
       </header>
-      <div className="max-h-[450px] overflow-y-auto overflow-x-hidden p-3">
+      <div className="max-h-[464px] overflow-y-auto overflow-x-hidden p-3">
         {books?.map((book, index) => (
           <BookCard key={index} book={book} />
         ))}
