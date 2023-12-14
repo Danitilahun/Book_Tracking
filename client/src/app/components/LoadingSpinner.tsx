4ec90ac247dff630e91590424462755e9a95e000
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-14 14:10:21 +0300

    adding loading spinner size

--- client/src/app/components/LoadingSpinner.tsx
+++ client/src/app/components/LoadingSpinner.tsx
@@ -3,7 +3,10 @@ import { Loader } from "lucide-react";
 const LoadingSpinner = () => {
   return (
     <div className="flex justify-center items-center h-full bg-transparent">
-      <Loader className="animate-spin min-h-[80vh]" />
+      <Loader
+        className="animate-spin min-h-[80vh]"
+        style={{ width: "60px", height: "60px" }}
+      />
     </div>
   );
 };
